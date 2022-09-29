1b7620c7f30e7c54238c5539d2f954d2200c2d37
Leonardo Luz <leonardoluz10@hotmail.com>
2022-09-29 11:01:24 -0300

    Adicionando veirificação de login valido e invalido

--- LoginCursoEmVideo.cy.js
+++ LoginCursoEmVideo.cy.js
@@ -2,5 +2,16 @@ describe('Validação de Login',()=>{
     beforeEach(()=>{
         cy.visit('https://www.cursoemvideo.com/')
     })
+    it('Verificando login invalido',()=>{
+       cy.login('Leonardo','12345678');
+       cy.on ('window:alert',(str)=>{
+        expect(str).to.equal('Error: The Username you have entered is Invalid')
+       })
+       
+    }) 
+    it.only('Verificando login valido',()=>{
+        cy.login('leonardoluz10@hotmail.com','HBalcasa18@');
+        cy.contains('a','Meu Painel').should('be.visible');
+    }) 
     
 })
