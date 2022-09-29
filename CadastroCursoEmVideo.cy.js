4f17c69b545bf167e296a324110ff404565f678d
Leonardo Luz <leonardoluz10@hotmail.com>
2022-09-29 09:57:01 -0300

    Adicionando a verificação de senha com menos de 8 caracteres

--- CadastroCursoEmVideo.cy.js
+++ CadastroCursoEmVideo.cy.js
@@ -11,12 +11,21 @@ describe('Validação de cadastro',()=>{
         cy.contains('span','Campo obrigatório.').should('be.visible');
         cy.contains('span','Campo obrigatório.').should('be.visible');
     })
-    it.only('Verificar mensagens de email invalido',()=>{
+    it('Verificar mensagens de email invalido',()=>{
         cy.contains('a','Cadastre-se').click();
         cy.contains('button','Registrar Gratuitamente').click();
         cy.get('input[name="user_email"]').type('Leonardo');
         cy.contains('span','Preencha com e-mail válido.').should('be.visible');
         
+    })
+    it('Verificar senha com menos de 8 caracteres',()=>{
+        cy.contains('a','Cadastre-se').click();
+        cy.contains('button','Registrar Gratuitamente').click();
+        cy.get('input[name="user_pass"]').type('1234567');
+        cy.contains('div','Muito fraca').should('be.visible');
+        cy.contains('button','Registrar Gratuitamente').click();
+        cy.contains('span','Your password should be at least 8 characters long.').should('be.visible');
+    
     })
 
 
